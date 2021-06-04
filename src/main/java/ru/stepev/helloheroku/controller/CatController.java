ff8e77c11c9149fef4ecc5718d0c503d32275dd7
Evgenii Stepanov <websafari@yandex.ru>
2021-06-04 19:27:29 +0300

    crud cat

--- src/main/java/ru/stepev/helloheroku/controller/CatController.java
+++ src/main/java/ru/stepev/helloheroku/controller/CatController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ru.stepev.helloheroku.model.Cat;
@@ -26,5 +29,44 @@ public class CatController {
 		model.addAttribute("cats", cats);
 		return "cats-page";
 	}
+	
+	@GetMapping("/{id}")
+	public String getCat(@PathVariable int id, Model model) {
+		Cat cat = catService.getById(id).orElseThrow();
+		model.addAttribute("cat", cat);
+		return "show-classroom";
+	}
+	
+	@GetMapping("/add")
+	public String addCat(Model model) {
+		Cat cat = new Cat();
+		model.addAttribute("cat", cat);
+		return "add-cat";
+	}
+	
+	@PostMapping("/create")
+	public String createCat(@ModelAttribute Cat cat, Model model) {
+		catService.add(cat);
+		return "redirect:/cats";
+	}
+	
+	@GetMapping("/update/{id}")
+	public String updateCat(@PathVariable int id, Model model) {
+		Cat cat = catService.getById(id).orElseThrow();
+		model.addAttribute("cat", cat);
+		return "update-cat";
+	}
+	
+	@PostMapping("/save")
+	public String saveCat(@ModelAttribute Cat cat, Model model) {
+		catService.update(cat);
+		return "redirect:/cats";
+	}
 
+	@GetMapping("/delete/{id}")
+	public String deleteCat(@PathVariable int id, Model model) {
+		Cat cat = catService.getById(id).orElseThrow();
+		catService.delete(cat);
+		return "redirect:/cats";
+	}
 }
--- src/main/java/ru/stepev/helloheroku/service/CatService.java
+++ src/main/java/ru/stepev/helloheroku/service/CatService.java
@@ -1,34 +1,40 @@
 package ru.stepev.helloheroku.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import lombok.AllArgsConstructor;
 import ru.stepev.helloheroku.model.Cat;
 import ru.stepev.helloheroku.repository.CatRepository;
 
 @Service
 //@AllArgsConstructor
 public class CatService {
-	
+
 	private CatRepository catRepository;
-	
-	public CatService (CatRepository catRepository) {
+
+	public CatService(CatRepository catRepository) {
 		this.catRepository = catRepository;
 	}
-	
+
 	public List<Cat> getAll() {
-//		List<Cat> cats = new ArrayList<>();
-//		cats.add(new Cat(1,"Vaka"));
-//		cats.add(new Cat(2,"Baka"));
-//		cats.add(new Cat(3,"Zuma"));
-//		cats.add(new Cat(4,"Puma"));
-//		cats.add(new Cat(5,"Oso"));
 		return catRepository.findAll();
 	}
-	
 
-	
+	public void add(Cat cat) {
+		catRepository.saveAndFlush(cat);
+	}
+
+	public void update(Cat cat) {
+		catRepository.saveAndFlush(cat);
+	}
+
+	public Optional<Cat> getById(int id) {
+		return catRepository.findById(id);
+	}
+
+	public void delete(Cat cat) {
+		catRepository.delete(cat);
+	}
 }
